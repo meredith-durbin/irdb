a7b9b286c285e0e890d23f182cb91f21d05d7547
teutoburg <ghost@instruct.at>
2026-02-05 16:00:31 +0100

    Refactor MICADO tests

--- MICADO/test_micado/test_micado_background_flux.py
+++ MICADO/test_micado/test_micado_background_flux.py
@@ -12,9 +12,9 @@ Comments
 # integration test using everything and the MICADO package
 from pathlib import Path
 import pytest
-from pytest import approx
 
 import numpy as np
+from numpy import testing as npt
 
 import scopesim as sim
 from scopesim import rc
@@ -40,7 +40,6 @@ class TestInit:
         assert len(opt_els) == 6
 
 
-@pytest.mark.xfail(reason="Background levels fail due to changes in dev_master.")
 class TestBackgroundLevels:
     """
     from Ric's excel doc 2018-04-03
@@ -50,35 +49,14 @@ class TestBackgroundLevels:
         0.6	5.0	    28.4	78.7
 
     """
+    @pytest.mark.parametrize("mode, factor", (("IMG_4mas", 1), ("IMG_1.5mas", (1.5 / 4)**2)))
     @pytest.mark.parametrize("fw1, fw2, bg_flux",
-                             [("J", "open", 5),
-                              ("open", "H", 30),
+                             [pytest.param("J", "open", 5, marks=pytest.mark.xfail(reason="Background levels fail due to changes in dev_master.")),
+                              pytest.param("open", "H", 30, marks=pytest.mark.xfail(reason="Background levels fail due to changes in dev_master.")),
                               ("open", "Ks", 79)])
-    def test_bg_SCAO_IMG_4mas(self, fw1, fw2, bg_flux):
-        cmds = sim.UserCommands(use_instrument="MICADO")
-        micado = sim.OpticalTrain(cmds)
-
-        micado["filter_wheel_1"].change_filter(fw1)
-        micado["filter_wheel_2"].change_filter(fw2)
-
-        src = empty_sky()
-        micado.observe(src)
-        implane = micado.image_planes[0].hdu.data       # e-/pixel/s
-
-        assert np.median(implane) == approx(bg_flux, rel=0.3)
-
-    @pytest.mark.parametrize("fw1, fw2, bg_flux",
-                             [("J", "open", 5),
-                              ("open", "H", 30),
-                              ("open", "Ks", 79)])
-    def test_bg_SCAO_IMG_1_5mas(self, fw1, fw2, bg_flux):
-        """
-        Pixel size is 0.14 of the 4mas version, hence fluxes are lower
-        """
-        bg_flux *= (1.5 / 4)**2
-
+    def test_bg_SCAO_IMG_4mas(self, mode, factor, fw1, fw2, bg_flux):
         cmds = sim.UserCommands(use_instrument="MICADO",
-                                set_modes=["SCAO", "IMG_1.5mas"])
+                                set_modes=["SCAO", mode])
         micado = sim.OpticalTrain(cmds)
 
         micado["filter_wheel_1"].change_filter(fw1)
@@ -86,6 +64,8 @@ class TestBackgroundLevels:
 
         src = empty_sky()
         micado.observe(src)
-        implane = micado.image_planes[0].hdu.data       # e-/pixel/s
+        implane = micado.image_planes[0].hdu.data  # e-/pixel/s
 
-        assert np.median(implane) == approx(bg_flux, rel=0.3)
+        # Pixel size is 0.14 of the 4mas version, hence fluxes are lower
+        bg_flux *= factor
+        npt.assert_allclose(np.median(implane), bg_flux, rtol=.3)
